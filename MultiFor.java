f0fe305902dd8e0f6aa45ec08102a2e03c2daa60
Sama872 <107773332+Sama872@users.noreply.github.com>
2022-06-21 13:20:03 -0700

    Add files via upload

---
+++ MultiFor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MultiFor;
+
+/**
+ *
+ * @author hp
+ */
+public class MultiFor {
+     public static void main(String [] args) {
+ for(int x = 0; x < 4; x++) {
+ 
+ for(int y = 4; y > 2; y--) {
+ System.out.println(x +" " + y);
+ }
+ if (x == 1) {  
+ } 
+ }
+ }
+    
+}
